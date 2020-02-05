59ffa746ef233fa027e0193671dd30553ab05f63
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-02-05 00:25:30 -0500

    Hide Maps API key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBHXKzGZEeBhP_m3QQ6vpI0hRODxeeEWl0")
+	NSString* mapsApiKey = [[NSProcessInfo processInfo] environment[@"MAPS_API_KEY"];
+	[GMSServices provideAPIKey:mapsApiKey];
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
