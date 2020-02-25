72f1d2e72d20ae37dc4add1040539afb873b18b3
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-02-25 02:07:41 -0500

    Create boilerplate models

---
+++ lib/models/reachable.dart
@@ -0,0 +1,9 @@
+import '../models/custom_location.dart';
+import 'direction.dart';
+
+/*
+ * From https://dart.dev/samples
+*/
+class Reachable {
+  List<Direction> pathTo(CustomLocation destination) {}
+}
