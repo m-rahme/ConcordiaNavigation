fb3d87bc5f803a3297f9fc8c7ebd7dc7c5519a5c
kajanthyS <kajanthy02@gmail.com>
2020-02-27 16:15:43 -0500

    added list of building stats

--- lib/widgets/building_information/building_stats.dart
+++ lib/widgets/building_information/building_stats.dart
@@ -2,14 +2,24 @@ import 'package:flutter/foundation.dart';
 
 class BuildingStatistics {
 
+  String building_initial;
   String building_name;
-
+  String campus_name;
+  String building_address;
+  double lat;
+  double long;
+  String op_hrs;
 
   BuildingStatistics({
+    @required this.building_initial,
     @required this.building_name,
-
-
+    @required this.campus_name,
+    @required this.lat,
+    @required this.long,
+    @required this.building_address,
+    this.op_hrs
 });
+
 
 
 }
---
+++ lib/widgets/building_widgets/building_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../building_information/building_stats.dart';
+
+class  BuildingList extends StatelessWidget {
+  final List <BuildingStatistics> build_stats;
+  BuildingList(this.build_stats);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
