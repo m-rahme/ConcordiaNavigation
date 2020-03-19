6d2b1f6940e9f92dd80ec9b09027889433564cd7
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-03-19 01:15:51 -0400

    #112 - Use ConsumerWidget

--- lib/services/location_search.dart
+++ lib/services/location_search.dart
@@ -16,97 +16,81 @@ class LocationSearch extends SearchDelegate {
     'MB1.437'
   ]; // for demonstration purposes
 
-
   ///This method returns suggested locations to the user, in this case Loyola and SGW campus.
   @override
   Widget buildSuggestions(BuildContext context) {
-
     final suggestionList = query.isEmpty
         ? recentRooms
         : classrooms.where((p) => p.contains(query.toUpperCase())).toList();
-    return ListView.builder(
-      itemBuilder: (context, index) => ListTile(
-        onTap: () async {
-          classrooms = json.decode(await rootBundle.loadString('assets/destinations.json'));
-          Provider.of<MapData>(context, listen: false)
-              .controllerDestination
-              .text = "SGW Campus, Montreal";
-          Provider.of<MapData>(context, listen: false).controllerStarting.text =
-              "Current Location";
-          Navigator.of(context).pop();
-          Provider.of<MapData>(context, listen: false)
-              .controllerDestination
-              .text = "SGW, Montreal";
-          Provider.of<MapData>(context, listen: false).controllerStarting.text =
-              "Current Location";
-          Provider.of<MapData>(context, listen: false).changeStart(
-              Provider.of<MapData>(context, listen: false).getCurrentLocation);
-          switch (suggestionList[index][0].toString()) {
-            case "H":
-              {
-                Provider.of<MapData>(context, listen: false)
-                    .changeCampus('sgw');
-                Provider.of<MapData>(context, listen: false)
-                    .changeEnd(constants.hBuilding);
-              }
-              break;
+    return Consumer<MapData>(builder: (context, mapData, child) {
+      return ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          onTap: () async {
+            classrooms = json.decode(
+                await rootBundle.loadString('assets/destinations.json'));
+            mapData.controllerDestination.text = "SGW Campus, Montreal";
+            mapData.controllerStarting.text = "Current Location";
+            Navigator.of(context).pop();
+            mapData.controllerDestination.text = "SGW, Montreal";
+            mapData.controllerStarting.text = "Current Location";
+            mapData.changeStart(mapData.getCurrentLocation);
+            switch (suggestionList[index][0].toString()) {
+              case "H":
+                {
+                  mapData.changeCampus('sgw');
+                  mapData.changeEnd(constants.hBuilding);
+                }
+                break;
 
-            case "M":
-              {
-                Provider.of<MapData>(context, listen: false)
-                    .changeCampus('sgw');
-                Provider.of<MapData>(context, listen: false)
-                    .changeEnd(constants.jmsbBuilding);
-              }
-              break;
+              case "M":
+                {
+                  mapData.changeCampus('sgw');
+                  mapData.changeEnd(constants.jmsbBuilding);
+                }
+                break;
 
-            case "L":
-              {
-                Provider.of<MapData>(context, listen: false)
-                    .changeCampus('loyola');
-                Provider.of<MapData>(context, listen: false)
-                    .changeEnd(constants.loyola);
-              }
-              break;
+              case "L":
+                {
+                  mapData.changeCampus('loyola');
+                  mapData.changeEnd(constants.loyola);
+                }
+                break;
 
-            case "J":
-              {
-                Provider.of<MapData>(context, listen: false)
-                    .changeCampus('sgw');
-                Provider.of<MapData>(context, listen: false)
-                    .changeEnd(constants.jmsbBuilding);
-              }
-              break;
+              case "J":
+                {
+                  mapData.changeCampus('sgw');
+                  mapData.changeEnd(constants.jmsbBuilding);
+                }
+                break;
 
-            default:
-              {
-                Provider.of<MapData>(context, listen: false)
-                    .changeCampus('sgw');
-                Provider.of<MapData>(context, listen: false)
-                    .changeEnd(constants.sgw);
-              }
-              break;
-          }
-          Provider.of<MapData>(context, listen: false).changeMode("driving");
-          Provider.of<MapData>(context, listen: false).setItinerary();
-        },
-        leading: Icon(Icons.location_city),
-        title: RichText(
-            text: TextSpan(
-                text: suggestionList[index].substring(0, query.length),
-                style: TextStyle(
-                    color: constants.blackColor, fontWeight: FontWeight.bold),
-                children: [
-              TextSpan(
-                text: suggestionList[index].substring(query.length),
-                style: TextStyle(
-                  color: constants.greyColor,
+              default:
+                {
+                  mapData.changeCampus('sgw');
+                  mapData.changeEnd(constants.sgw);
+                }
+                break;
+            }
+            mapData.changeMode("driving");
+            mapData.setItinerary();
+          },
+          leading: Icon(Icons.location_city),
+          title: RichText(
+              text: TextSpan(
+                  text: suggestionList[index].substring(0, query.length),
+                  style: TextStyle(
+                      color: constants.blackColor, fontWeight: FontWeight.bold),
+                  children: [
+                TextSpan(
+                  text: suggestionList[index].substring(query.length),
+                  style: TextStyle(
+                    color: constants.greyColor,
+                  ),
                 ),
-              ),
-            ])),
-      ),
-      itemCount: suggestionList.length,
-    );
+              ])),
+        ),
+        itemCount: suggestionList.length,
+      );
+    });
   }
 
   ///This method adds a return IconButton to return to the homepage.
