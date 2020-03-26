b4e12cab15b38f336e5f7b9e8259e4615dbeb0a8
Alex <ahayeur@gmail.com>
2020-03-26 18:21:23 -0400

    Adds framework for fixing #78
    Requires Frontend overhaul

--- lib/screens/shuttle_schedule.dart
+++ lib/screens/shuttle_schedule.dart
@@ -15,15 +15,203 @@ class ShuttleSchedule extends StatelessWidget {
       ),
       body: DataTable(
         columns: [
-          DataColumn(label: Text('SGW to Loyola')),
+
+          DataColumn(label: Text('Loyola Departure')),
+          DataColumn(label: Text('Loyola Fri')),
+          DataColumn(label: Text('SGW Departure')),
+          DataColumn(label: Text('SGW Fri'))
         ],
         rows: [
           DataRow(
             selected: true,
             cells: [
+              DataCell(Text('07:30')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:35')),
+              DataCell(Text('07:45')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('08:15')),
+              DataCell(Text('08:05')),
+              DataCell(Text('08:20')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:55')),
+              DataCell(Text('08:15')),
+              DataCell(Text('08:20')),
+              DataCell(Text('08:55')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('08:20')),
+              DataCell(Text('09:10')),
+              DataCell(Text('08:35')),
+              DataCell(Text('09:30')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('08:35')),
+              DataCell(Text('09:10')),
+              DataCell(Text('08:55')),
+              DataCell(Text('09:30')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('08:55')),
+              DataCell(Text('10:20')),
+              DataCell(Text('09:10')),
+              DataCell(Text('10:05')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('09:10')),
+              DataCell(Text('10:35')),
+              DataCell(Text('09:30')),
+              DataCell(Text('10:55')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('09:30')),
+              DataCell(Text('11:10')),
+              DataCell(Text('09:45')),
+              DataCell(Text('11:10')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('09:45')),
+              DataCell(Text('11:30')),
+              DataCell(Text('10:05')),
+              DataCell(Text('11:45')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('10:20')),
+              DataCell(Text('11:45')),
+              DataCell(Text('10:20')),
+              DataCell(Text('12:05')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('10:35')),
+              DataCell(Text('12:20')),
+              DataCell(Text('10:55')),
+              DataCell(Text('12:20')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('10:55')),
+              DataCell(Text('12:40')),
+              DataCell(Text('11:10')),
+              DataCell(Text('12:55')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('11:10')),
+              DataCell(Text('12:55')),
+              DataCell(Text('11:45')),
+              DataCell(Text('13:30')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('11:30')),
+              DataCell(Text('13:30')),
+              DataCell(Text('12:15')),
+              DataCell(Text('13:45')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
               DataCell(Text('07:40')),
             ],
           ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+            ],
+          ),
+          DataRow(
+            selected: true,
+            cells: [
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+              DataCell(Text('07:40')),
+            ],
+          ),
+
         ],
       ),
     );
