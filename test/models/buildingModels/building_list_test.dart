4373969aec39633d579d03afb9e47082383438ed
EvanMateo <gevanmateo@gmail.com>
2020-03-27 01:39:08 -0400

    Update unit test building_list_test.dart

--- test/models/buildingModels/building_list_test.dart
+++ test/models/buildingModels/building_list_test.dart
@@ -1,5 +1,5 @@
 import 'package:concordia_navigation/models/buildingModels/building_list.dart';
-import 'package:flutter/services.dart' show AssetBundle;
+import 'package:flutter/services.dart' show AssetBundle, rootBundle;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -9,10 +9,19 @@ class MockAssetBundle extends Mock implements AssetBundle{}
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   group('BuildingList', () {
-    test('readBuildingFile() creates list of BuildingInformation with a specific length', () async {
+    test('readBuildingFile() creates list of BuildingInformation with the same length as the asset file.', () async {
+      
+      // Build list
       BuildingList buildingList = new BuildingList();
       await buildingList.readBuildingFile();
-      expect(buildingList.getListOfBuildings().length, 10);
+
+      // Get number of lines from source
+      String file = await rootBundle.loadString('assets/campus_buildings_info.txt');
+      String linebreak = '\n';
+      int numLines = linebreak.allMatches(file).length;
+
+      // Compare lengths
+      expect(buildingList.getListOfBuildings().length, numLines);
     });
   });
 }
