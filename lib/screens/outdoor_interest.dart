513f3516eb0c8012073614594cef1d04233034f3
wayne1tam <waynevick7226@hotmail.com>
2020-03-26 21:57:58 -0400

    #35 - Created 2 tabs for POI and reorganized code
    1- New POI widget file poi_display.dart
    This widget holds the list of the poi
    
    2- Tabs in outdoor_interest.dart
    There are now 2 tabs to list the poi for the respective campus.
    The old list that displays the poi was moved to the new POI widget file
    called poi_display.dart. It now only needs the snapshot and the campus
    string. A back button was added on the app bar. Direction buttons were
    added.
    
    Missing styling of the widgets.

--- lib/screens/outdoor_interest.dart
+++ lib/screens/outdoor_interest.dart
@@ -1,6 +1,7 @@
 import 'package:concordia_navigation/models/outdoor_poi/outdoor_poi.dart';
 import 'package:concordia_navigation/models/outdoor_poi/outdoor_poi_list.dart';
 import 'package:concordia_navigation/storage/app_constants.dart';
+import 'package:concordia_navigation/widgets/poi_display.dart';
 import 'package:flutter/material.dart';
 import 'package:concordia_navigation/services/localization.dart';
 
@@ -13,87 +14,44 @@ class OutdoorInterest extends StatelessWidget {
   Future<List<OutdoorPOI>> callAsyncFetch() {
     return Future.delayed(Duration(seconds: 2), () => outdoorPOIList.readPOIFile());
   }
-
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(ConcordiaLocalizations.of(context).interest),
-        backgroundColor: greenColor),
-      body: FutureBuilder(
-        future: callAsyncFetch(),
-        builder: (context, AsyncSnapshot<List<OutdoorPOI>> snapshot){
-          if(snapshot.hasData){
-            return Container(
-
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: Row(children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-
-                          Text(
-                            snapshot.data[index].getName(),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                              color: Colors.black,
-                            ),
-                          ),
-
-                          Text(
-                            snapshot.data[index].getAddress(),
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.grey,
-                            ),
-                          ),
-
-                          Text(
-                            "Open: " +snapshot.data[index].getOpen() + " Close: "+snapshot.data[index].getClose(),
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.grey,
-                            ),
-                          ),
-
-                        ],
-
-                      ),
-
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-
-                          Container(
-                              decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2),),
-                              child: new Image.asset(
-                                snapshot.data[index].getLogo(),
-                                height: 80.0,
-                                fit: BoxFit.cover,
-
-                              )
-                          ),
-
-                        ],
-                      ),
-
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: new IconButton(
+              icon: new Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            title: Text(ConcordiaLocalizations.of(context).interest),
+            backgroundColor: greenColor,
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "Sir George Williams"),
+                Tab(text: "Loyola"),
+              ],
+            ),
+          ),
+          body:
+              FutureBuilder(
+                future: callAsyncFetch(),
+                builder: (context, AsyncSnapshot<List<OutdoorPOI>> snapshot){
+                if(snapshot.hasData){
+                  return  TabBarView(
+                    children: [
+                        new POI_Display(snapshot: snapshot, campus: "Sir George Williams",),
+                        new POI_Display(snapshot: snapshot, campus: "Loyola",),
                     ],
-                    ),
                   );
-                },
-                itemCount: snapshot.data.length,
+                }
+                else return Center(child: CircularProgressIndicator());
+                }
               ),
-            );
-          }
-          else{
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+        ),
+      )
     );
   }
 }
