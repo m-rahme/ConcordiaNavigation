851919447a6ad861c7db1f765a0a2f0665563716
EvanMateo <gevanmateo@gmail.com>
2020-03-28 08:32:27 -0400

    #92 - Update widget tests for custom drawer and all its screens + remove widget test
    Removed custom_appbar_test.dart and custom_drawer_test.dart since they're covered in app_widget_test.dart

--- test/app_widget_test.dart
+++ test/app_widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:concordia_navigation/widgets/custom_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'app_widget.dart';
@@ -5,14 +6,24 @@ import 'app_widget.dart';
 
 // TODO: Update this test once each screen have been implemented
 void main() {
+  /**
+   * Open drawer when in homepage
+   */
   openDrawer(WidgetTester tester) async {
     await tester.dragFrom(
         tester.getTopLeft(find.byType(MaterialApp)), Offset(300, 0));
     await tester.pumpAndSettle();
   }
 
+  /**
+   * 1. Tap finder
+   * 2. Tap center button
+   * 3. Go back to map
+   * 4. Re-open drawer
+   */
   verifyScreen(WidgetTester tester, String name) async {
     final finder = find.text(name);
+    expect(finder, findsOneWidget);
     await tester.tap(finder);
     await tester.pumpAndSettle();
     final btn = find.byType(RaisedButton);
@@ -36,24 +47,41 @@ void main() {
       // Open drawer
       await openDrawer(tester);
 
-      // Verify
+      // Verify campus
       final campus = find.text("Campus");
       expect(campus, findsOneWidget);
-
       await tester.tap(campus);
       await tester.pumpAndSettle();
 
       final sgw = find.text("Sir George Williams");
       expect(sgw, findsOneWidget);
+      await tester.tap(sgw);
+      await tester.pumpAndSettle();
+      await openDrawer(tester);
+      await tester.tap(campus);
+      await tester.pumpAndSettle();
 
       final loyola = find.text("Loyola");
       expect(loyola, findsOneWidget);
+      await tester.tap(loyola);
+      await tester.pumpAndSettle();
+      await openDrawer(tester);
+      await tester.tap(campus);
+      await tester.pumpAndSettle();
 
-      // Verify each screens
+      // Verify all other screens from drawer
       await verifyScreen(tester, "Schedule");
       await verifyScreen(tester, "Outdoor Interest");
       await verifyScreen(tester, "Profile");
       await verifyScreen(tester, "Settings");
+
+      // Verify drawer closes by tapping on drawer header
+      final drawer = find.byType(CustomDrawer); 
+      expect(drawer, findsOneWidget);
+      final header = find.text("ConNavigation").at(1);
+      await tester.tap(header);
+      await tester.pumpAndSettle();
+      expect(drawer, findsNothing);
     });
   });
 }
