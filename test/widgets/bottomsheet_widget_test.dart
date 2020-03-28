fe8b8a0f51c306c4daf885b37b1d75a89f2076d9
EvanMateo <gevanmateo@gmail.com>
2020-03-28 13:30:37 -0400

    #92 - Create unit test for bottomsheet_widget_test.dart

---
+++ test/widgets/bottomsheet_widget_test.dart
@@ -0,0 +1,49 @@
+import 'package:concordia_navigation/models/buildingModels/building_information.dart';
+import 'package:concordia_navigation/services/size_config.dart';
+import 'package:concordia_navigation/widgets/bottomsheet_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('BottomSheetWidget', () {
+    testWidgets('creates bottom sheet with BuildingInformation',
+        (WidgetTester tester) async {
+      BuildingInformation buildingInformation = BuildingInformation(
+        campusName: "campusName",
+        buildingName: "buildingName",
+        buildingInitial: "buildingInitial",
+        buildingAddress: "buildingAddress",
+        latitude: 90.0,
+        longitude: 90.0,
+        filename: "filename",
+      );
+
+      final testWidget = MediaQuery(
+          data: new MediaQueryData(),
+          child: MaterialApp(
+            home: Scaffold(
+              body: TestWidget(buildingInformation)
+            ),
+          ));
+
+      await tester.pumpWidget(testWidget);
+
+      await tester.pumpAndSettle();
+      final campusName = find.text("Directions");
+      expect(campusName, findsOneWidget);
+    });
+  });
+}
+
+class TestWidget extends StatelessWidget {
+  BuildingInformation buildingInformation;
+
+  TestWidget(this.buildingInformation);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return BottomSheetWidget(this.buildingInformation);
+  }
+}
