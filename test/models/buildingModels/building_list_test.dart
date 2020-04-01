5d923f371f70055c4730fb4c52a8b2426b72c2e9
wayne1tam <waynevick7226@hotmail.com>
2020-03-31 22:18:36 -0400

    #124 - Edited Tests related to buildings

--- test/models/buildingModels/building_list_test.dart
+++ test/models/buildingModels/building_list_test.dart
@@ -18,11 +18,14 @@ void main() {
       BuildingList buildingList = new BuildingList();
 
       // Get number of lines from source
-//      String linebreak = '\n';
-//      int numLines = linebreak.allMatches(BuildingList.buildingInfo).length;
-//
-//      // Compare lengths
-//      expect(buildingList.getListOfBuildings().length, numLines);
+
+      int loyolaBuidings = BuildingList.buildingInfo['Loyola'].length;
+      int sgwBuildings = BuildingList.buildingInfo['Sir George Williams'].length;
+
+      int numOfBuildings = buildingList.getListOfBuildings().length;
+
+
+      expect(loyolaBuidings + sgwBuildings, numOfBuildings);
     });
   });
 }
