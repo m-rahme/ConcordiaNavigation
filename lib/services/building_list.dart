6ff4c27ae5da0d2109aef5f4ec695d0535902129
wayne1tam <waynevick7226@hotmail.com>
2020-04-01 00:07:49 -0400

    #124 - Refactored the json file and building list reader
    The json file was restructured to simplify the method in the building
    list reader so that it could be easily iterate.

--- lib/services/building_list.dart
+++ lib/services/building_list.dart
@@ -2,35 +2,39 @@ import 'dart:async' show Future;
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:concordia_navigation/models/building.dart';
-
 class BuildingList {
   final Set<Building> buildingList = Set<Building>();
-  static Map<String, dynamic> buildingInfo;
-
-  BuildingList() {_readBuildingFile();}
-
-  static Future<Map<String, dynamic>> loadAsset() async {
-    return json.decode(await rootBundle.loadString('assets/campus_buildings_info.json'));
+  static List<dynamic> buildingInfo;
+  BuildingList() {
+    _readBuildingFile();
   }
-
+  static Future<List<dynamic>> loadAsset() async {
+    return json.decode(
+        await rootBundle.loadString('assets/campus_buildings_info.json'));
+  }
   void _readBuildingFile() async {
-    for(int campus = 0; campus< buildingInfo.keys.length; campus++){
-      for(int attribute = 0; attribute< buildingInfo[buildingInfo.keys.elementAt(campus)].keys.length; attribute++){
-        this.buildingList.add(
-            Building(
-                campusName: buildingInfo.keys.elementAt(campus),
-                buildingName: buildingInfo[buildingInfo.keys.elementAt(campus)][buildingInfo[buildingInfo.keys.elementAt(campus)].keys.elementAt(attribute)]['buildingName'],
-                buildingInitial: buildingInfo[buildingInfo.keys.elementAt(campus)][buildingInfo[buildingInfo.keys.elementAt(campus)].keys.elementAt(attribute)]['buildingInitial'],
-                buildingAddress: buildingInfo[buildingInfo.keys.elementAt(campus)][buildingInfo[buildingInfo.keys.elementAt(campus)].keys.elementAt(attribute)]['buildingAddress'],
-                latitude: double.parse(buildingInfo[buildingInfo.keys.elementAt(campus)][buildingInfo[buildingInfo.keys.elementAt(campus)].keys.elementAt(attribute)]['latitude']),
-                longitude: double.parse(buildingInfo[buildingInfo.keys.elementAt(campus)][buildingInfo[buildingInfo.keys.elementAt(campus)].keys.elementAt(attribute)]['longitude']),
-                filename: buildingInfo[buildingInfo.keys.elementAt(campus)][buildingInfo[buildingInfo.keys.elementAt(campus)].keys.elementAt(attribute)]['Logo'],
-            )
-        );
+    for (int campusIndex = 0;
+    campusIndex < BuildingList.buildingInfo.length;
+    campusIndex++) {
+
+      for (int buildingIndex = 0;
+      buildingIndex <
+          BuildingList.buildingInfo[campusIndex]['buildings'].length;
+      buildingIndex++) {
+        Map<String, dynamic> building =
+        BuildingList.buildingInfo[campusIndex]['buildings'][buildingIndex];
+        this.buildingList.add(Building(
+          campusName: buildingInfo[campusIndex]['campusIndexName'],
+          buildingName: building['buildingName'],
+          buildingInitial: building['buildingInitial'],
+          buildingAddress: building['buildingAddress'],
+          latitude: double.parse(building['latitude']),
+          longitude: double.parse(building['longitude']),
+          filename: building['filename'],
+        ));
       }
     }
   }
-
   Set<Building> getListOfBuildings() {
     return buildingList;
   }
