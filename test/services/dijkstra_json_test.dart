496dcce38c8732d63f497aa254d040c91f3a9cc6
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-05 16:24:22 -0400

    #34 - Tidy up test

--- test/services/dijkstra_json_test.dart
+++ test/services/dijkstra_json_test.dart
@@ -1,12 +1,10 @@
 import 'package:concordia_navigation/models/node.dart';
 import 'package:concordia_navigation/models/outdoor/campus.dart';
 import 'package:concordia_navigation/services/dijkstra.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Dijkstra', () {
-    //WidgetsFlutterBinding.ensureInitialized();
     TestWidgetsFlutterBinding.ensureInitialized();
     List<dynamic> campusData;
     Map<String, Node> graph;
@@ -14,7 +12,6 @@ void main() {
     List<Node> solution;
 
     setUp(() async {
-      // Mock data
       campusData = await Campus.loadJson();
       graph = {};
 
@@ -22,16 +19,12 @@ void main() {
           ?.forEach((building) => building['floors']?.forEach((floor) {
                 if (floor['classrooms'] != null && floor['poi'] != null) {
                   floor['classrooms'].forEach((classroom) {
-                    if (classroom['name'] == null) {
-                      print(1);
-                    } else {
+                    if (classroom['name'] != null) {
                       graph[classroom['name']] = Node(classroom['name']);
                     }
                   });
                   floor['poi'].forEach((poi) {
-                    if (poi['name'] == null) {
-                      print(1);
-                    } else {
+                    if (poi['name'] != null) {
                       graph[poi['name']] = Node(poi['name']);
                     }
                   });
@@ -41,52 +34,34 @@ void main() {
       campusData.forEach((campus) => campus['buildings']
           ?.forEach((building) => building['floors']?.forEach((floor) {
                 if (floor['classrooms'] != null) {
-                  // print(floor['classrooms']);
                   floor['classrooms']?.forEach((classroom) {
                     if (classroom['edges'] != null) {
                       classroom['edges'].forEach((edge) {
-                        if (edge['name'] != null) {
-                          print("${classroom['name']} ${edge['name']}");
-                          if (edge['name'] == null) print(1);
+                        if (edge['name'] != null)
                           graph[classroom['name']]
                               .setEdge(graph[edge['name']], edge['distance']);
-                        }
                       });
                     }
                   });
                   floor['poi']?.forEach((poi) {
                     if (poi['edges'] != null) {
                       poi['edges'].forEach((edge) {
-                        if (edge['name'] != null) {
-                          print("${poi['name']} ${edge['name']}");
-                          if (edge['name'] == null) print(1);
+                        if (edge['name'] != null)
                           graph[poi['name']]
                               .setEdge(graph[edge['name']], edge['distance']);
-                        }
                       });
                     }
                   });
                 }
               })));
 
-      print(graph);
-
-      solution = [graph['entrance'], graph['checkMM'], graph['escalatorUp'], graph['exitMR']];
+      solution = [
+        graph['entrance'],
+        graph['checkMM'],
+        graph['escalatorUp'],
+        graph['exitMR']
+      ];
       dijkstra = Dijkstra.fromGraph(graph);
-
-      // campusData.forEach((campus) => campus['buildings'].forEach((building) =>
-      //     building['floors']?.forEach((floor) => floor['classrooms']?.forEach(
-      //         (classroom) =>
-
-      // print(graph);
-
-      // campusData.forEach((campus) => campus['buildings'].forEach((building) =>
-      //     building['floors']?.forEach((floor) => floor['classrooms']?.forEach(
-      //         (classroom) => classroom['edges'].forEach((edge) =>
-      // graph[classroom['name']]
-      //     .setEdge(graph[edge['name']], edge['distance']))))));
-
-      //dijkstra = Dijkstra.fromGraph(graph);
     });
 
     test('constructor creates a list of edges', () {
