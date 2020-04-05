34219519291855789a3240ed936ceb0b1ca6122d
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-04 20:56:45 -0400

    #34 - Add Campus model

---
+++ lib/models/campus.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:concordia_navigation/models/building.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Campus {
+  String name;
+  String initials;
+  LatLng latlng;
+  Set<Building> buildings = {};
+
+  Campus.fromJson(Map json) {
+    this.name = json['campusName'];
+    this.initials = json['campusInitials'];
+
+    for (int bIndex = 0; bIndex < json['buildings'].length; bIndex++)
+      buildings.add(Building.fromJson(json['buildings'][bIndex]));
+  }
+
+  ///For reading the file
+  static Future<List<dynamic>> loadJson() async =>
+      json.decode(await rootBundle.loadString('assets/campus_buildings.json'));
+}
