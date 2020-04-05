aaa3ecb55b812efcad8fc201493c33520ec91c03
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-04 20:58:04 -0400

    #34 - Add fromJson() constructor + remove `final`

--- lib/models/floor.dart
+++ lib/models/floor.dart
@@ -2,8 +2,19 @@ import 'package:concordia_navigation/models/classroom.dart';
 import 'package:concordia_navigation/models/indoor_interest.dart';
 
 class Floor {
-  final int floorNumber;
-  final Set<Classroom> classrooms;
-  final Set<IndoorInterest> indoorInterests;
-  Floor({this.floorNumber, this.classrooms, this.indoorInterests});
+  int floorNumber;
+
+  // TODO: one set of IndoorLocation and use polymorphism
+  Set<Classroom> classrooms = {};
+  Set<IndoorInterest> indoorInterests = {};
+
+  Floor.fromJson(Map json) {
+    this.floorNumber = int.parse(json['number']);
+
+    for (int i = 0; i < json['classrooms'].length; i++)
+      classrooms.add(Classroom.fromJson(json['classrooms'][i]));
+
+    for (int i = 0; i < json['classrooms'].length; i++)
+      indoorInterests.add(IndoorInterest.fromJson(json['classrooms'][i]));
+  }
 }
