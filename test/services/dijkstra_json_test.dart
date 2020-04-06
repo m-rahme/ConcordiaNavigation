323379a83d0ed09d5b33b2f06ddc0be03acad71e
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-06 05:00:21 -0400

    #34 - Use Dijkstra.fromJson() constructor + fix names

--- test/services/dijkstra_json_test.dart
+++ test/services/dijkstra_json_test.dart
@@ -13,59 +13,20 @@ void main() {
 
     setUp(() async {
       campusData = await Campus.loadJson();
-      graph = {};
+      dijkstra= Dijkstra.fromJson(campusData);
+      graph = dijkstra.nodes;
 
-      campusData.forEach((campus) => campus['buildings']
-          ?.forEach((building) => building['floors']?.forEach((floor) {
-                if (floor['classrooms'] != null && floor['poi'] != null) {
-                  floor['classrooms'].forEach((classroom) {
-                    if (classroom['name'] != null) {
-                      graph[classroom['name']] = Node(classroom['name']);
-                    }
-                  });
-                  floor['poi'].forEach((poi) {
-                    if (poi['name'] != null) {
-                      graph[poi['name']] = Node(poi['name']);
-                    }
-                  });
-                }
-              })));
-
-      campusData.forEach((campus) => campus['buildings']
-          ?.forEach((building) => building['floors']?.forEach((floor) {
-                if (floor['classrooms'] != null) {
-                  floor['classrooms']?.forEach((classroom) {
-                    if (classroom['edges'] != null) {
-                      classroom['edges'].forEach((edge) {
-                        if (edge['name'] != null)
-                          graph[classroom['name']]
-                              .setEdge(graph[edge['name']], edge['distance']);
-                      });
-                    }
-                  });
-                  floor['poi']?.forEach((poi) {
-                    if (poi['edges'] != null) {
-                      poi['edges'].forEach((edge) {
-                        if (edge['name'] != null)
-                          graph[poi['name']]
-                              .setEdge(graph[edge['name']], edge['distance']);
-                      });
-                    }
-                  });
-                }
-              })));
 
       solution = [
-        graph['entrance'],
-        graph['checkMM'],
-        graph['escalatorUp'],
-        graph['exitMR']
+        graph['entranceH1'],
+        graph['checkMMH1'],
+        graph['escalatorUpH1'],
+        graph['exitMRH1']
       ];
-      dijkstra = Dijkstra.fromGraph(graph);
     });
 
     test('constructor creates a list of edges', () {
-      List<Node> result = dijkstra.pathTo("entrance", "exitMR");
+      List<Node> result = dijkstra.pathTo("entranceH1", "exitMRH1");
       expect(solution, result);
     });
   });
