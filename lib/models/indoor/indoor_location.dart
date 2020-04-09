494b8fda7715f7014de7a2b38e223af3363087c3
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 19:33:55 -0400

    #166 - Add IndoorLocation abstract class
    This class models an indoor location. It's reachable because some of its parents are reachable.

--- lib/models/indoor/indoor_location.dart
+++ lib/models/indoor/indoor_location.dart
@@ -1,15 +1,42 @@
+import 'package:concordia_navigation/models/indoor/classroom.dart';
 import 'package:concordia_navigation/models/indoor/coordinate.dart';
+import 'package:concordia_navigation/models/indoor/floor.dart';
+import 'package:concordia_navigation/models/outdoor/building.dart';
+import 'package:concordia_navigation/models/reachable.dart';
+import 'package:concordia_navigation/models/uni_location.dart';
+import 'package:concordia_navigation/services/search.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-abstract class IndoorLocation {
-  String name;
+/// indoor locations are reachable since theyre in buildings and buildings are reachable
+abstract class IndoorLocation extends UniLocation implements Reachable {
   Coordinate room;
   Coordinate nearest;
 
-  IndoorLocation({this.name, this.room, this.nearest});
+  IndoorLocation(String name, {UniLocation parent, this.room, this.nearest})
+      : super(name, parent: parent);
 
-  IndoorLocation.fromJson(Map json)
-      : assert(json['name'] != null), // required
-        name = json['name'],
-        room = Coordinate.fromJson(json['coordinates']['room']),
-        nearest = Coordinate.fromJson(json['coordinates']['nearest']);
+  IndoorLocation.fromJson(Map json, {UniLocation parent})
+      : room = Coordinate.fromJson(json['coordinates']['room']),
+        nearest = Coordinate.fromJson(json['coordinates']['nearest']),
+        super(json['name'], parent: parent) {
+    // skip floors as users shouldn't be able to look those in the search bar
+    if (this is Classroom) Search.supported.add(this);
+  }
+
+  @override
+  double get lat {
+    Floor floor = parent as Floor;
+    Building building = floor.parent as Building;
+    return building.lat;
+  }
+
+  @override
+  double get long {
+    Floor floor = parent as Floor;
+    Building building = floor.parent as Building;
+    return building.lat;
+  }
+
+  @override
+  LatLng toLatLng() => lat == null || long == null ? null : LatLng(lat, long);
 }
