d367e5720cb9d6995b78320a565b6eae8808fe03
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 19:47:18 -0400

    #166 - Add parameters

--- lib/models/indoor/indoor_poi.dart
+++ lib/models/indoor/indoor_poi.dart
@@ -1,5 +1,8 @@
 import 'package:concordia_navigation/models/indoor/indoor_location.dart';
+import 'package:concordia_navigation/models/uni_location.dart';
 
 class IndoorPOI extends IndoorLocation {
-  IndoorPOI.fromJson(json): super.fromJson(json);
+  IndoorPOI.fromJson(json,
+      {UniLocation parent, double latitude, double, longitude})
+      : super.fromJson(json, parent: parent);
 }
