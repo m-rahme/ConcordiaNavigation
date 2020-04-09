ea8855f5cff9aa00e70aae31512a0f7ff98e5705
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 19:51:18 -0400

    #166 - Use IndoorItinerary

--- lib/providers/indoor_data.dart
+++ lib/providers/indoor_data.dart
@@ -1,9 +1,20 @@
-import 'package:concordia_navigation/models/indoor/indoor_location.dart';
-import 'package:concordia_navigation/services/dijkstra.dart';
+import 'package:concordia_navigation/services/outdoor/indoor_itinerary.dart';
 import 'package:flutter/material.dart';
 
 class IndoorData extends ChangeNotifier {
-  static Dijkstra shortest;
+  IndoorItinerary _indoorItinerary;
 
-  static Map<String, IndoorLocation> indoors = {};
+  IndoorData();
+
+  IndoorItinerary get indoorItinerary => _indoorItinerary;
+
+  void setItinerary(String start, String end) {
+    _indoorItinerary = IndoorItinerary(start, end);
+    notifyListeners();
+  }
+
+  void removeItinerary() {
+    _indoorItinerary = null;
+    notifyListeners();
+  }
 }
