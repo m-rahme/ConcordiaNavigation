41d77d511b815c8dd924beb09a00790edff0409c
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 19:41:53 -0400

    #166 - Refactor Campus class
    This refactor makes use of the OOP structure from previous commits.

--- lib/models/outdoor/campus.dart
+++ lib/models/outdoor/campus.dart
@@ -1,25 +1,35 @@
-import 'dart:convert';
 import 'package:concordia_navigation/models/outdoor/building.dart';
-import 'package:flutter/services.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:concordia_navigation/models/outdoor/outdoor_location.dart';
+import 'package:concordia_navigation/models/outdoor/outdoor_poi.dart';
+import 'package:concordia_navigation/models/university.dart';
 
-class Campus {
-  String name;
-  String initials;
-  LatLng latlng;
-  Set<Building> buildings = {};
+// Concrete outdoor location
+class Campus extends OutdoorLocation {
+  final String initials;
   static Campus sgw;
   static Campus loy;
+  final List<OutdoorPOI> interests = [];
 
-  Campus.fromJson(Map json) {
-    this.name = json['campusName'];
-    this.initials = json['campusInitials'];
+  Campus._create(String name, this.initials, {University parent})
+      : super(name, parent: parent);
 
-    for (int bIndex = 0; bIndex < json['buildings'].length; bIndex++)
-      buildings.add(Building.fromJson(json['buildings'][bIndex]));
+  factory Campus.fromJson(Map<String, dynamic> json, {University parent}) {
+    if (json['campusName'] == null) return null;
+    Campus c = Campus._create(json['campusName'], json['campusInitials'],
+        parent: parent);
+    List<Building> buildings = [];
+    for (int bIndex = 0; bIndex < json['buildings'].length; bIndex++) {
+      Building temp = Building.fromJson(c, json['buildings'][bIndex]);
+      if (temp != null) buildings.add(temp);
+    }
+    c.children = buildings;
+
+    for (int iIndex = 0; iIndex < json['interests'].length; iIndex++) {
+      OutdoorPOI temp =
+          OutdoorPOI.fromJson(json['interests'][iIndex], parent: c);
+      if (temp != null) c.interests.add(temp);
+    }
+
+    return c;
   }
-
-  ///For reading the file
-  static Future<List<dynamic>> loadJson() async =>
-      json.decode(await rootBundle.loadString('assets/campus_buildings.json'));
 }
