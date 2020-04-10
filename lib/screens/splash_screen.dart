f8e36542dacddea642a864c19cdf81bab10f0ed0
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 20:12:30 -0400

    #166 - Remove getBytesFromAsset(String, int)

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -18,16 +18,6 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-Future<Uint8List> getBytesFromAsset(String path, int width) async {
-  ByteData data = await rootBundle.load(path);
-  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
-      targetWidth: width);
-  ui.FrameInfo fi = await codec.getNextFrame();
-  return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
-      .buffer
-      .asUint8List();
-}
-
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   AnimationController _animationController;
