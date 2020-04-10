ea2d02cc1ad2b7873c0e47b121d6e86b5af723d3
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 20:19:57 -0400

    [dep] - Upgrade Dependencies

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -19,7 +19,6 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    //LoadBuildingInfo();
     Painters();
     return Scaffold(
       appBar: HomePageAppBar(),
