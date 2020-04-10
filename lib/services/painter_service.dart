687730baa1c807c3a537325888000ed5acaa698b
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 20:47:36 -0400

    #166 - Indoor with new hierarchy
    This commit starts implementing the indoor feature using the class hierarchy from previous commits

--- lib/services/painter_service.dart
+++ lib/services/painter_service.dart
@@ -13,91 +13,18 @@ class PainterService extends CustomPainter {
     paint.color = Colors.red;
     paint.style = PaintingStyle.stroke;
     paint.strokeWidth = 3.0;
-    String startLocation = "";
-    String endLocation = "";
-    Set<IndoorLocation> locH1 = new Set<IndoorLocation>();
-    Set<IndoorLocation> locH8 = new Set<IndoorLocation>();
-    Set<IndoorLocation> locH9 = new Set<IndoorLocation>();
-    Set<IndoorLocation> locMB = new Set<IndoorLocation>();
-
-    List<Node> result = IndoorData.shortest.pathTo(startLocation, endLocation);
 
     Path path = Path();
 
-    for (Node node in result) {
-      switch (node.name.substring(0, 2)) {
-        case "H1":
-          locH1.add(IndoorData.indoors[node.name]);
-          break;
-        case "H8":
-          locH8.add(IndoorData.indoors[node.name]);
-          break;
-        case "H9":
-          locH9.add(IndoorData.indoors[node.name]);
-          break;
-        case "MB":
-          locMB.add(IndoorData.indoors[node.name]);
-          break;
-      }
-    }
+    for (IndoorLocation indoor in itinerary.path) {
+      if (indoor == itinerary.path.first)
+        path.moveTo((940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
 
-    switch (index) {
-      case 0:
-        for (IndoorLocation indoor in locH1) {
-          if (indoor == locH1.first) {
-            path.moveTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-          path.lineTo(
-              (940 * indoor.nearest.x) / 1000, (862 * indoor.nearest.y / 920));
-          if (indoor == locH1.last) {
-            path.lineTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-        }
-        break;
-      case 1:
-        for (IndoorLocation indoor in locH8) {
-          if (indoor == locH8.first) {
-            path.moveTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-          path.lineTo(
-              (940 * indoor.nearest.x) / 1000, (862 * indoor.nearest.y / 920));
-          if (indoor == locH8.last) {
-            path.lineTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-        }
-        break;
-      case 2:
-        for (IndoorLocation indoor in locH9) {
-          if (indoor == locH9.first) {
-            path.moveTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-          path.lineTo(
-              (940 * indoor.nearest.x) / 1000, (862 * indoor.nearest.y / 920));
-          if (indoor == locH9.last) {
-            path.lineTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-        }
-        break;
-      case 3:
-        for (IndoorLocation indoor in locMB) {
-          if (indoor == locMB.first) {
-            path.moveTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-          path.lineTo(
-              (940 * indoor.nearest.x) / 1000, (862 * indoor.nearest.y / 920));
-          if (indoor == locMB.last) {
-            path.lineTo(
-                (940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
-          }
-        }
-        break;
+      path.lineTo(
+          (940 * indoor.nearest.x) / 1000, (862 * indoor.nearest.y / 920));
+
+      if (indoor == itinerary.path.last)
+        path.lineTo((940 * indoor.room.x) / 1000, (862 * indoor.room.y / 920));
     }
 
     canvas.drawPath(path, paint);
