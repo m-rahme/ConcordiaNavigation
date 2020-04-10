b611079a4058f3f192f6ff9db0b63dc2178a13be
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 20:00:10 -0400

    #166 - Add search service
    This service's main function `query(String)` searches for an element in the list of supported elements for navigation.
    
    The text parameter should be upper cased before it is used as every element's string is upper cased for comparison.
    
    The `indoor(Reachable)` function searches for an indoor location in the list of supported locations and returns its .toLatLng().

---
+++ lib/services/search.dart
@@ -0,0 +1,30 @@
+import 'package:concordia_navigation/models/indoor/floor.dart';
+import 'package:concordia_navigation/models/indoor/indoor_location.dart';
+import 'package:concordia_navigation/models/reachable.dart';
+import 'package:concordia_navigation/models/uni_location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+enum query_result { unsupported, invalid, supported }
+
+class Search {
+  // for getting latlng's
+  static List<dynamic> supported = [];
+  // in case anybody needs it
+  static List<UniLocation> everything = [];
+  // for searching
+  static List<String> names = [];
+
+  /// Search for everything but floors
+  static dynamic query(String text) => Search.supported.firstWhere(
+      (object) => object is! Floor && object.name.toUpperCase().contains(text),
+      orElse: () => null);
+
+  /// Granular search
+  static LatLng indoor(Reachable location) {
+    if (location is IndoorLocation && !supported.contains(location))
+      throw Exception("sorry location not found or unsupported");
+    else {
+      return location.toLatLng();
+    }
+  }
+}
