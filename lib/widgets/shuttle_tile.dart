9caf6b7f6c908952b6af5922ad431cd7f10ac3f0
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-09 20:24:38 -0400

    #166 - Add commented code to use hierarchy

--- lib/widgets/shuttle_tile.dart
+++ lib/widgets/shuttle_tile.dart
@@ -1,17 +1,29 @@
+import 'package:concordia_navigation/models/indoor/indoor_location.dart';
+import 'package:concordia_navigation/models/outdoor/building.dart';
+import 'package:concordia_navigation/models/uni_location.dart';
 import 'package:concordia_navigation/providers/map_data.dart';
 import 'package:concordia_navigation/services/outdoor/shuttle_service.dart';
 import 'package:concordia_navigation/storage/app_constants.dart' as constants;
 import 'package:flutter/material.dart';
 import 'package:concordia_navigation/services/size_config.dart';
-import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 //Tile with shuttle information used in the shuttle widget
 class ShuttleTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String campus = Provider.of<MapData>(context, listen: false).getCampus;
-    String shuttleTime = ShuttleService.getNextShuttle(campus);
+    // TODO: fix to use element's campus
+    // var mapData = Provider.of<MapData>(context, listen: false);
+    // String campus;
+    // if (mapData.end is Building) {
+    //   campus = (mapData.end as UniLocation).parent.name;
+    // } else if (mapData.end is IndoorLocation) {
+    //   campus = (mapData.end as UniLocation).parent.parent.name;
+    // } else {
+    //   campus = "loy";
+    // }
+    String shuttleTime = ShuttleService.getNextShuttle("sgw");
 
     return Container(
       decoration: BoxDecoration(
