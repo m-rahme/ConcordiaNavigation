087f31f58b1d809d1bc590397748c9a43258528b
Michel <michel.rahmeh@gmail.com>
2020-04-11 03:51:32 -0400

    #164 - Ignore previous 2 commits

--- lib/widgets/outdoor/map_page.dart
+++ lib/widgets/outdoor/map_page.dart
@@ -17,13 +17,9 @@ class MapPage extends StatelessWidget {
       margin: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 18.0),
       controller: Provider.of<MapData>(context, listen: false).panelController,
       maxHeight: SizeConfig.safeBlockVertical * 85,
-      minHeight: (Provider.of<MapData>(context, listen: false)
-                      .controllerStarting !=
-                  null ||
-              Provider.of<MapData>(context, listen: false).controllerEnding !=
-                  null)
-          ? SizeConfig.safeBlockVertical * 24
-          : SizeConfig.safeBlockVertical * 0,
+      minHeight: Provider.of<MapData>(context).itinerary == null
+          ? SizeConfig.safeBlockVertical * 0
+          : SizeConfig.safeBlockVertical * 24,
       defaultPanelState: PanelState.CLOSED,
       backdropEnabled: true,
       backdropOpacity: 0.3,
