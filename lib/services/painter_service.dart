0f1ba5f3832f065187a02e80033148db2d99e240
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-11 01:21:34 -0400

    #164 - Add funcitoning building to building directions

--- lib/services/painter_service.dart
+++ lib/services/painter_service.dart
@@ -1,3 +1,4 @@
+import 'package:concordia_navigation/models/indoor/floor.dart';
 import 'package:concordia_navigation/models/indoor/indoor_location.dart';
 import 'package:concordia_navigation/services/outdoor/indoor_itinerary.dart';
 import 'package:concordia_navigation/storage/app_constants.dart' as constants;
@@ -17,23 +18,43 @@ class PainterService extends CustomPainter {
 
     Path path = Path();
 
+    Map<int, List<IndoorLocation>> map = {0: [], 1: [], 2: [], 3: []};
+
+    print(itinerary);
+
+    for (IndoorLocation indoor in itinerary.path)
+      map[(indoor.parent as Floor).page].add(indoor);
+
     ///Calculations made so match container size where the svg is built with the svg itself
     ///This way, the path will be drawn on the right X and Y coordinates
-    for (IndoorLocation indoor in itinerary.path) {
-      if (indoor == itinerary.path.first)
-        path.moveTo(
-            (constants.containerWidth * indoor.room.x) / constants.svgWidth,
-            (constants.containerHeight * indoor.room.y / constants.svgHeight));
-
-      path.lineTo(
-          (constants.containerWidth * indoor.nearest.x) / constants.svgWidth,
-          (constants.containerHeight * indoor.nearest.y / constants.svgHeight));
-
-      if (indoor == itinerary.path.last)
-        path.lineTo(
-            (constants.containerWidth * indoor.room.x) / constants.svgWidth,
-            (constants.containerHeight * indoor.room.y / constants.svgHeight));
-    }
+    /// check if an indoor location is in a floor
+    /// if it is and the floor matches the index, draw it
+    map.forEach((key, list) {
+      if (key == index) {
+        list.forEach((indoor) {
+          if (indoor == list.first) {
+            path.moveTo(
+                (constants.containerWidth * indoor.room.x) / constants.svgWidth,
+                (constants.containerHeight *
+                    indoor.room.y /
+                    constants.svgHeight));
+          }
+          path.lineTo(
+              (constants.containerWidth * indoor.nearest.x) /
+                  constants.svgWidth,
+              (constants.containerHeight *
+                  indoor.nearest.y /
+                  constants.svgHeight));
+          if (indoor == list.last) {
+            path.lineTo(
+                (constants.containerWidth * indoor.room.x) / constants.svgWidth,
+                (constants.containerHeight *
+                    indoor.room.y /
+                    constants.svgHeight));
+          }
+        });
+      }
+    });
 
     canvas.drawPath(path, paint);
   }
