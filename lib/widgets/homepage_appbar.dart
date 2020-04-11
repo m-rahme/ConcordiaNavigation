3ec21a26bb0ecf691a6b1ceec532d8a5c8b77bf7
Michel <michel.rahmeh@gmail.com>
2020-04-10 20:33:22 -0400

    #164 - Add indoor button in appbar

--- lib/widgets/homepage_appbar.dart
+++ lib/widgets/homepage_appbar.dart
@@ -1,7 +1,9 @@
+import 'package:concordia_navigation/providers/map_data.dart';
 import 'package:concordia_navigation/screens/indoor_page.dart';
 import 'package:concordia_navigation/storage/app_constants.dart' as constants;
 import 'package:flutter/material.dart';
-import 'package:concordia_navigation/services/location_search.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 //Custom AppBar shown only in HomePage
 class HomePageAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -11,18 +13,25 @@ class HomePageAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: constants.appColor,
       title: Text("ConNavigation"),
       actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.domain),
-          tooltip: "Go Indoor!",
-          onPressed: () {
-            Navigator.pushNamed(context, '/indoor',
-                arguments: Arguments(false));
-          },
+        Center(
+          child: InkWell(
+            child: Text(
+              "Indoor",
+              style: GoogleFonts.raleway(
+                  fontWeight: FontWeight.w600, fontSize: 18.0),
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/indoor',
+                  arguments: Arguments(false));
+            },
+          ),
         ),
+        SizedBox(width: 7.0),
         IconButton(
           icon: Icon(Icons.search),
           onPressed: () {
-            showSearch(context: context, delegate: LocationSearch());
+//            showSearch(context: context, delegate: LocationSearch());
+            Provider.of<MapData>(context, listen: false).togglePanel();
           },
         )
       ],
