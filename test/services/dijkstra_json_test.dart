a8b930746a6c080a32f408f7d00b6ba588c26406
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-11 16:45:52 -0400

    #113 - Add tests with accessibility

--- test/services/dijkstra_json_test.dart
+++ test/services/dijkstra_json_test.dart
@@ -13,20 +13,101 @@ void main() {
 
     setUp(() async {
       campusData = await University.loadJson();
-      dijkstra= Dijkstra.fromJson(campusData);
+      dijkstra = Dijkstra.fromJson(campusData);
       graph = dijkstra.nodes;
-
-
+    });
+    test('constructor creates a list of edges', () {
+      solution = [
+        graph['H1entrance'],
+        graph['H1escalatorDown'],
+        graph['H1exitMR']
+      ];
+      List<Node> result = dijkstra.pathTo("H1entrance", "H1exitMR");
+      expect(solution, result);
+    });
+    test('testing route with accessibility disabled', () {
       solution = [
         graph['H1entrance'],
         graph['H1checkMM'],
         graph['H1escalatorUp'],
-        graph['H1exitMR']
+        graph['H8escalatorUp'],
+        graph['H862'],
+        graph['H859'],
+        graph['H857'],
+        graph['H855'],
       ];
+      List<Node> result =
+          dijkstra.pathTo("H1entrance", "H855", accessible: false);
+      expect(solution, result);
     });
-
-    test('constructor creates a list of edges', () {
-      List<Node> result = dijkstra.pathTo("H1entrance", "H1exitMR");
+    test('testing route with accessibility enabled', () {
+      solution = [
+        graph['H1entrance'],
+        graph['H1checkMM'],
+        graph['H1women'],
+        graph['H1elevator'],
+        graph['H8elevator'],
+        graph['H859'],
+        graph['H857'],
+        graph['H855'],
+      ];
+      List<Node> result =
+          dijkstra.pathTo("H1entrance", "H855", accessible: true);
+      expect(solution, result);
+    });
+    test(
+        'testing route from entrance of Hall to H967 with accessibility disabled',
+        () {
+      solution = [
+        graph['H1entrance'],
+        graph['H1checkMM'],
+        graph['H1escalatorUp'],
+        graph['H9escalatorUp'],
+        graph['H9escalatorDown'],
+        graph['H962'],
+        graph['H964'],
+        graph['H963'],
+        graph['H965'],
+        graph['H967'],
+      ];
+      List<Node> result =
+          dijkstra.pathTo("H1entrance", "H967", accessible: false);
+      expect(solution, result);
+    });
+    test(
+        'testing route from entrance of Hall to H967 with accessibility enabled',
+        () {
+      solution = [
+        graph['H1entrance'],
+        graph['H1checkMM'],
+        graph['H1women'],
+        graph['H1elevator'],
+        graph['H9elevator'],
+        graph['H962'],
+        graph['H964'],
+        graph['H963'],
+        graph['H965'],
+        graph['H967'],
+      ];
+      List<Node> result =
+          dijkstra.pathTo("H1entrance", "H967", accessible: true);
+      // for (Node node in result) print(node.name);
+      expect(solution, result);
+    });
+    test('testing route from H110 to H913 with accessibility enabled', () {
+      solution = [
+        graph['H110'],
+        graph['H1checkMM'],
+        graph['H1women'],
+        graph['H1elevator'],
+        graph['H9elevator'],
+        graph['H9accessibility'],
+        graph['H909'],
+        graph['H911'],
+        graph['H913'],
+      ];
+      List<Node> result = dijkstra.pathTo("H110", "H913", accessible: true);
+      for (Node node in result) print(node.name);
       expect(solution, result);
     });
   });
