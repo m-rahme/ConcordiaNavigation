839661315a98ccec63fd6ed832bd95ddca1373b5
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-11 15:04:03 -0400

    #113 - Add accessible attribute
    This attribute is used when computing paths with accessibility enabled.

--- lib/models/node.dart
+++ lib/models/node.dart
@@ -3,8 +3,10 @@ class Node implements Comparable<Node> {
   int distance;
   Map<Node, int> edges = {};
   Node previous;
+  bool accessible;
 
-  Node(this.name, [this.distance = 9999]);
+  Node(this.name, [this.distance = 9999])
+      : accessible = !name.contains('escalator');
 
   /// checks if the map is already
   void setEdge(Node node, int distance) => edges[node] = distance;
