f6cc6fc6c12771d9409dcd9ea41d5abb29d2a815
EvanMateo <gevanmateo@gmail.com>
2020-04-11 00:32:31 -0400

    #147 - Create widget test for course_schedule.dart + modify CourseSchedule to make it testable

--- test/app_widget.dart
+++ test/app_widget.dart
@@ -3,12 +3,6 @@ import 'package:concordia_navigation/models/user_location.dart';
 import 'package:concordia_navigation/providers/buildings_data.dart';
 import 'package:concordia_navigation/providers/calendar_data.dart';
 import 'package:concordia_navigation/providers/map_data.dart';
-import 'package:concordia_navigation/screens/course_schedule.dart';
-import 'package:concordia_navigation/screens/home_page.dart';
-import 'package:concordia_navigation/screens/outdoor_interest.dart';
-import 'package:concordia_navigation/screens/profile.dart';
-import 'package:concordia_navigation/screens/settings.dart';
-import 'package:concordia_navigation/screens/shuttle_schedule.dart';
 import 'package:concordia_navigation/services/localization.dart';
 import 'package:concordia_navigation/services/location_service.dart';
 import 'package:concordia_navigation/services/size_config.dart';
@@ -18,7 +12,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 
 /// Widget used to test whole App
-Widget appWidget({MapData mockMapData, Widget testWidget}) {
+Widget appWidget({MapData mockMapData, CalendarData mockCalendarData, Widget testWidget}) {
   TestWidgetsFlutterBinding.ensureInitialized();
 // Providers
   Widget userLocationProvider = StreamProvider<UserLocation>(
@@ -39,9 +33,11 @@ Widget appWidget({MapData mockMapData, Widget testWidget}) {
   );
 
 // Mock Providers
-  if(mockMapData != null) {
+  if(mockMapData != null) 
     mapDataProvider = ChangeNotifierProvider<MapData>.value( value: mockMapData,);
-  }
+  if(mockCalendarData != null)
+    calendarData = ChangeNotifierProvider<CalendarData>.value( value: mockCalendarData,);
+  
 
 // Testing App
   return MultiProvider(
---
+++ test/screens/course_schedule_test.dart
@@ -0,0 +1,52 @@
+import 'package:concordia_navigation/models/course.dart';
+import 'package:concordia_navigation/models/schedule.dart';
+import 'package:concordia_navigation/providers/calendar_data.dart';
+import 'package:concordia_navigation/screens/course_schedule.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import '../app_widget.dart';
+
+class MockCalendarData extends Mock implements CalendarData{
+  Future<Schedule> retrieveFromDevice() => null;
+}
+
+void main() {
+  group('CourseSchedule', () {
+    MockCalendarData mockCalendarData = new MockCalendarData();
+    Schedule schedule;
+    DateTime start;
+    DateTime end;
+    DateTime now;
+
+    
+
+    setUp(() {
+      start = DateTime(2020, 1, 1, 10, 30);
+      end = DateTime(2020, 1, 1, 11, 30);
+      now = DateTime(2020, 1, 1);
+      schedule = Schedule.fromJson({
+                  'summary': 'Schedule summary',
+                  'timeZone': 'America/Toronto',
+                  'items': [
+                    {
+                      'summary': 'Test course',
+                      'description': 'Test description',
+                      'start': {'dateTime': start.toString()},
+                      'end': {'dateTime': end.toString()},
+                      'location': 'H420'
+                    }
+                  ]
+                  });
+      when(mockCalendarData.schedule).thenReturn(schedule);
+    });
+    testWidgets('course is displayed', (WidgetTester tester) async {
+      await tester.pumpWidget(appWidget(mockCalendarData: mockCalendarData, testWidget: CourseSchedule(now: now,)));
+      await tester.pumpAndSettle();
+
+      final weekday = find.text("Wednesday");
+      final courseLocation = find.text("Test course");
+      expect(weekday, findsOneWidget);
+      expect(courseLocation, findsOneWidget);
+    });
+  });
+}
