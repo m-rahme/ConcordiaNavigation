784eae2b14c12be92220244052fee7dbbee35049
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-11 16:45:08 -0400

    #113 - Create new dijkstra object for every route

--- lib/providers/indoor_data.dart
+++ lib/providers/indoor_data.dart
@@ -1,3 +1,6 @@
+import 'package:concordia_navigation/models/indoor/indoor_location.dart';
+import 'package:concordia_navigation/models/university.dart';
+import 'package:concordia_navigation/services/dijkstra.dart';
 import 'package:concordia_navigation/services/outdoor/indoor_itinerary.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +14,13 @@ class IndoorData extends ChangeNotifier {
   void toggleWheelchair() {
     wheelchair = !wheelchair;
     if (_indoorItinerary != null) {
-      setItinerary(
-          start: _indoorItinerary.path.first.name,
-          end: _indoorItinerary.path.last.name,
-          accessible: wheelchair);
+      University.loadJson().then((data) {
+        Dijkstra.shortest = Dijkstra.fromJson(data);
+        setItinerary(
+            start: _indoorItinerary.path.first.name,
+            end: _indoorItinerary.path.last.name,
+            accessible: wheelchair);
+      });
     }
     notifyListeners();
   }
--- lib/services/dijkstra.dart
+++ lib/services/dijkstra.dart
@@ -66,30 +66,13 @@ class Dijkstra {
         _pq = PriorityQueue<Node>();
 
   List<Node> pathTo(String start, String end, {bool accessible = false}) {
-    _compute(start, accessible);
-
-    Queue<Node> inOrder = Queue<Node>();
-    inOrder.addFirst(_nodes[end]);
-    bool stop = false;
-    while (!stop) {
-      if (_nodes[end].previous == null) {
-        stop = true;
-      } else {
-        inOrder.addFirst(_nodes[end].previous);
-        end = _nodes[end].previous?.name;
-        stop = end == start;
-      }
-    }
-
-    return inOrder.toList();
-  }
-
-  void _compute(String start, bool accessible) {
     // distance from start to start is 0
-    _nodes[start].distance = 0;
+    Map<String, Node> _temp = Map.from(_nodes);
+
+    _temp[start].distance = 0;
 
     // distance for each node
-    _nodes.forEach((key, node) {
+    _temp.forEach((key, node) {
       if (key != start) {
         node.distance = 9999;
       }
@@ -97,19 +80,38 @@ class Dijkstra {
       _pq.add(node);
     });
 
+    // if (accessible) {
+    //   disable('escalator');
+    // } else {
+    //   disable('accessible');
+    // }
+
     if (accessible) {
-      _nodes.forEach((key, og) {
+      _temp.forEach((key, og) {
         if (!og.accessible) {
-          print(og.name);
-          og.edges.forEach((key, val) {
-            if (!key.accessible) {
-              og.edges[key] = 9999;
-            }
+          og.edges.forEach((key2, val) {
+            og.edges[key2] = 99999;
           });
-          _nodes.forEach((key, node) {
+          _temp.forEach((key, node) {
             node.edges.forEach((thing, otherThing) {
               if (thing == og) {
-                thing.edges[thing] = 9999;
+                thing.edges[thing] = 99999;
+              }
+            });
+          });
+        }
+      });
+    } else {
+      _temp.forEach((key, badNode) {
+        if (badNode.name.contains('accessibility') ||
+            badNode.name.contains('elevator')) {
+          badNode.edges.forEach((checkNode, val) {
+            badNode.edges[checkNode] = 99999;
+          });
+          _temp.forEach((goodNodeName, goodNode) {
+            goodNode.edges.forEach((maybeBadNode, maybeBadDistance) {
+              if (maybeBadNode == badNode) {
+                goodNode.edges[maybeBadNode] = 99999;
               }
             });
           });
@@ -120,14 +122,29 @@ class Dijkstra {
     while (_pq.isNotEmpty) {
       Node u = _pq.removeFirst();
       u.edges.forEach((v, distance) {
-        int newDistance = _nodes[u.name].distance + u.edges[v];
+        int newDistance = _temp[u.name].distance + u.edges[v];
         if (newDistance < v.distance) {
           v.distance = newDistance;
           v.previous = u;
-          _nodes[v.name] = v;
+          _temp[v.name] = v;
           _pq.add(v);
         }
       });
     }
+
+    Queue<Node> inOrder = Queue<Node>();
+    inOrder.addFirst(_temp[end]);
+    bool stop = false;
+    while (!stop) {
+      if (_temp[end].previous == null) {
+        stop = true;
+      } else {
+        inOrder.addFirst(_temp[end].previous);
+        end = _temp[end].previous?.name;
+        stop = end == start;
+      }
+    }
+
+    return inOrder.toList();
   }
 }
